4f171a1e77459614ea89e5377d486742a2f27ef6
Carlos Antonio <carlos_antonio.minga@outlook.com>
2022-09-16 09:42:17 -0300

    Upload Project - Instalaçao Tailwind e configuraçao dos arquivos, pastas e imagens

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,9 @@
-//React Sintaxe: JSX (Javascrip + XML)
-// permite escrever html dentro da funçao Js, componentes.
-
-interface ButtonProps{
-  title : string
-}
-
-function Button(propiedades: ButtonProps){
-  return (
-    <button>
-       {propiedades.title}
-    </button>
-  )
-}
+import './styles/main.css';
 
 function App() {
     return (
-      <div>
-        <Button title="Enviar 1"/>
-        <Button title="Hello World"/>
+      <div className='w-8 h-8 bg-violet-500'>
+        
       </div>
     )
 }
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    // setar cores por padrao
+    extend: {}
+  },
+  plugins: []
+};
