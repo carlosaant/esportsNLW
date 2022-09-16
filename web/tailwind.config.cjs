29597f41f377d79e8563185220e8f4e351d99e0d
Carlos Antonio <carlos_antonio.minga@outlook.com>
2022-09-16 10:53:26 -0300

    style-web: adicionado propriedades css config tailwind, adicionado logo e texto, realocado arquivo background

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: [
+    './src/**/*.tsx',
+    // como esta sendo colocado classe tailwind dentro do html, precisa declarar aqui no content tambem como um arquivo de conteudo
+    './index.html'
+  ],
+
   theme: {
-    // setar cores por padrao
-    extend: {}
+    // setar cores
+    // setar a imagem de fundo diretamente
+    // criar propriedades
+    extend: {
+      backgroundImage: {
+        //enviar para pasta Public, para poder ser acessado publicamente pelo endereço
+        backgroundGalaxy: "url('/background-galaxy.png')",
+        'nlw-gradient':
+          'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%);'
+      }
+    }
   },
   plugins: []
 };
