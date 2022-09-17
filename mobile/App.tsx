195a2265a9b53525db755f9981364d58119d3da0
carlosaant <carlos_antonio.minga@outlook.com>
2022-09-17 12:48:32 -0300

    mobile: importado fonts, instalado dependecias necessarias

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,24 @@
 import {StatusBar} from 'react-native';
 // statusbar(barra superior com informaçoes de hora, bateria etc) sobrepor a aplicaçao, melhorando a experiencia do usuario
 
+import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter';
+// importando a fonts a serem utilizada que foram instaladas
+
 import { Background } from './src/components/Background';
 
 
 // ---------------------------------
 
 export default function App() {
+  // inicializando as fontes que vao ser utilizadas
+ const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black
+  });
+  
+
   return (
     <Background>
       <StatusBar 
@@ -14,7 +26,10 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-         
+        {
+          // garantir que as fonts foram carregadas, antes da execução
+          fontsLoaded ? 
+        }
       
     </Background>
   );
